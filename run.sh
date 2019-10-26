fe21d39e1383da91a001a4932b3fac60be003ec7
zouri <sunguoshuai@git.builderx.com>
2019-10-26 09:30:53 +0800

    ✨ 增加运行脚本

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # 拷贝配置文件到新的目录，如果存在将会跳过
-cp -r /etc/nps_conf /tmp/nps_conf
+if [ ! -d "/tmp/nps_conf" ]; then
+    mkdir /tmp/nps_conf
+fi
+cp -r /etc/nps_conf_tmp/* /tmp/nps_conf
 mv -n /tmp/nps_conf/* /nps/conf
 
 # execution
